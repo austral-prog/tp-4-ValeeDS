0a46af42b53a14e772fb5bb1fbde51d41d8e7c3a
Valeria Del Savio <valeria.delsavio@hotmail.com>
2025-04-11 12:42:53 -0300

    update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    año = int(input("Ingrese un año: "))
+    result = "es"
+
+    if año % 100 == 0 and año % 400 == 0:
+        result = "es"
+    elif año % 100 != 0 and año % 4 == 0:
+        result = "es"
+    else:
+        result = "no es"
+
+    print(f"El año {año} {result} bisiesto")
