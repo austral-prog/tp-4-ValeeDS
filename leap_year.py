a2bd163cb0603923af98d34cda9cb89ba696d7ad
Valeria Del Savio <valeria.delsavio@hotmail.com>
2025-04-11 12:44:25 -0300

    simplify leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -2,7 +2,7 @@ def leap_year():
     año = int(input("Ingrese un año: "))
     result = "es"
 
-    if año % 100 == 0 and año % 400 == 0:
+    if año % 400 == 0:
         result = "es"
     elif año % 100 != 0 and año % 4 == 0:
         result = "es"
